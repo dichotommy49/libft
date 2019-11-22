6408d206ab7ee80d6d77764c4465798dd4f4ec05
dichotommy49 <56877004+dichotommy49@users.noreply.github.com>
2019-11-22 14:00:53 +0100

    Changed conditions in ft_realloc
    The work continues

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: tmelvin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 16:40:31 by tmelvin           #+#    #+#             */
-/*   Updated: 2019/11/22 13:44:26 by tmelvin          ###   ########.fr       */
+/*   Updated: 2019/11/22 13:59:38 by tmelvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ void	*ft_realloc(void *ptr, size_t current_size, size_t new_size)
 {
 	void	*tmp;
 
-	if (ptr && new_size == 0)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	else if (!ptr)
+		return (malloc(new_size));
 	if (!(tmp = malloc(new_size)))
 	{
 		free(ptr);
