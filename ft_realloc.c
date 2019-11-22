a0ba27a4e5c1cfc21c9acfef5536f193ef749193
dichotommy49 <56877004+dichotommy49@users.noreply.github.com>
2019-11-22 12:33:45 +0100

    Malloc protection of ft_realloc() now frees ptr

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: tmelvin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 16:40:31 by tmelvin           #+#    #+#             */
-/*   Updated: 2019/11/21 16:46:24 by tmelvin          ###   ########.fr       */
+/*   Updated: 2019/11/22 12:31:45 by tmelvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	*ft_realloc(void *ptr, size_t size)
 	void	*tmp;
 
 	if (!(tmp = malloc(size)))
+	{
+		free(ptr);
 		return (NULL);
+	}
 	ft_memcpy(tmp, ptr, size);
 	free(ptr);
 	return (tmp);
