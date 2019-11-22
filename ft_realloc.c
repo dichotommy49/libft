5d9d6b2c4eff7d1501d4001cf139f70d6b9dbb81
dichotommy49 <56877004+dichotommy49@users.noreply.github.com>
2019-11-22 13:44:54 +0100

    Further changes to realloc
    Because nothing is ever easy...

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,22 +6,30 @@
 /*   By: tmelvin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 16:40:31 by tmelvin           #+#    #+#             */
-/*   Updated: 2019/11/22 12:31:45 by tmelvin          ###   ########.fr       */
+/*   Updated: 2019/11/22 13:44:26 by tmelvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_realloc(void *ptr, size_t size)
+void	*ft_realloc(void *ptr, size_t current_size, size_t new_size)
 {
 	void	*tmp;
 
-	if (!(tmp = malloc(size)))
+	if (ptr && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	ft_memcpy(tmp, ptr, size);
+	if (!(tmp = malloc(new_size)))
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > current_size)
+		ft_memcpy(tmp, ptr, current_size);
+	else
+		ft_memcpy(tmp, ptr, new_size);
 	free(ptr);
 	return (tmp);
 }
